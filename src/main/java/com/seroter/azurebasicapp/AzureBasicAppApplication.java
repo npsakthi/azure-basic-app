a4047f1703635b47d1fbdfae8489fc56ca7c2b4e
npsakthi <psakthivel@ceiamerica.com>
2020-08-24 21:09:28 +0530

    url added

--- src/main/java/com/seroter/azurebasicapp/AzureBasicAppApplication.java
+++ src/main/java/com/seroter/azurebasicapp/AzureBasicAppApplication.java
@@ -9,5 +9,13 @@ public class AzureBasicAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AzureBasicAppApplication.class, args);
 	}
+	
+	@Value("${company:Not configured by a Spring Cloud Server}")
+    private String company;
+     
+    @GetMapping("/hello")
+    public String Hello() {
+      return "hello, from " + company;
+    }
 
 }
